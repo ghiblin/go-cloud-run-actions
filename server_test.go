4b10ae0ad9e907a7af62cf02dd92cef5cb03718f
Alessandro Vitali <alessandro.vitali@consensys.net>
2022-11-25 16:01:24 +0100

    first commit

---
+++ server_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestHelloWo(t *testing.T) {
+	var expected Phrase
+
+	expected.Text = "Hello, world"
+	result := HelloWorld()
+
+	if expected.Text != result.Text {
+		t.Errorf("Phrase was incorrect. Got: %s, want: %s.", result.Text, expected.Text)
+	}
+}
